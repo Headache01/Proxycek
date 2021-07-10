82fa9f5ee2f142c0f5e4b04406cdc9420f0df7b4
İsmail <84140360+Headache01@users.noreply.github.com>
2021-07-10 12:17:06 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate():
-    ad = ["Ahmet", "Mehmet", "Ali"]
-    soyad = ["Çilli", "Bakır", "Gevrek"]
-    return "{} {}".format(random.choice(ad), random.choice(soyad))
-
-for i in range(2):
-    print(generate())
